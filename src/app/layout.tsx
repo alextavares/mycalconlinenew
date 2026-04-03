a8a5a7295ac76d85071f67529f04921b78382847
alextavares <alexandretmoraes110@gmail.com>
2026-04-02 22:00:10 -0300

    feat: add adsense verification snippet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ export default function RootLayout({
     <html lang="en" className="overflow-x-hidden">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2455351614806042" crossOrigin="anonymous"></script>
       </head>
       <body className="overflow-x-hidden">
         {children}
